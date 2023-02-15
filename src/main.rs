e5e2cb32358bea61baf35f5421716b5778191144
Anthony4m <amoahanthony41@gmail.com>
2023-02-15 22:22:33 +0000

    Added player

--- src/main.rs
+++ src/main.rs
@@ -76,3 +76,19 @@ enum GameMode{
     Play,
     End,
 }
+
+struct Player{
+    x:i32,
+    y:i32,
+    velocity:f32,
+}
+
+impl Player {
+    fn new (x:i32,y:i32) -> Self{
+        Player{
+            x,
+            y,
+            velocity:0.0,
+        }
+    }
+}
